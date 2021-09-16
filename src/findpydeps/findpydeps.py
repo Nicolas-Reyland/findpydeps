595fa73c2bd6a90fdec82aa782dede8ade7312b7
Nicolas Reyland <nicolas.reyland@epita.fr>
2021-09-16 18:19:33 +0200

    added future imports for backwards compat for python

--- src/findpydeps/findpydeps.py
+++ src/findpydeps/findpydeps.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 from __future__ import annotations
+from __future__ import print_function
+from __future__ import with_statement
 
 # Find Python Dependencies
 from argparse import ArgumentParser
